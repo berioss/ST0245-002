7acf752da4b20dd614b04abed873ef7efd662952
berioss <68913258+berioss@users.noreply.github.com>
2020-07-30 18:50:51 -0500

    Add files via upload

--- talleres/taller01/Taller1/Linea2D.java
+++ talleres/taller01/Taller1/Linea2D.java
@@ -1,28 +1,71 @@
-import java.util.Random;
+
+/**
+ * Write a description of class Linea2D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */import java.util.Random;
 import java.util.ArrayList;
 public class Linea2D
 {
-    Punto puntoInicial;
-    Punto puntoFinal;
+    static Punto puntoInicial;
+    static Punto puntoFinal;
+    static ArrayList<Punto> puntos;
 
-    public Linea2D(Punto PI,Punto PF){
+    public static ArrayList<Punto> Linea(Punto P1,Punto P2){
+        Punto PI;
+        Punto PF;
+        if(P1.X()<=P2.X()){PI=P1;PF=P2;}
+        else{PI=P2;PF=P1;}
+
+        puntos=new ArrayList<>(0);
+        puntos.add(PI);
         puntoInicial=PI;
         puntoFinal=PF;
-    }
+        int h=(int)(puntoFinal.Y())-(int)(puntoInicial.Y());
+        int v=(int)(puntoFinal.X())-(int)(puntoInicial.X());
+        int dt;
+        int d;
+        int cambioV;
+        int cambioH;
+        if(v>=0)cambioV=1;
+        else cambioV =-1;
+        if(h>=0)cambioH=1;
+        else cambioH =-1;
+        h=Math.abs(h);
+        v=Math.abs(v);
+        int a=(h+v)-1;
 
-    public ArrayList<Punto> puntosIntermedios(){
-        double m=(puntoFinal.Y()-puntoInicial.Y())/(puntoFinal.X()-puntoInicial.X());
-        double b=puntoInicial.Y()-m*puntoInicial.X();
-        double d=puntoFinal.X()-puntoInicial.X();
-        ArrayList<Punto> puntos = new ArrayList<>(1);
-        puntos.add(puntoInicial);
-        for(int i=1;i<d;i++){
-            if(((puntoInicial.X()+i)*m+b)%1==0){
-                puntos.add(new Punto(puntoInicial.X()+i,((puntoInicial.X()+i)*m+b)));
+        if(v>=h){
+            dt=v-h;
+                d=dt;
+            for(int i=0;i<a;i++){
+                
+                if(v-dt/2>h){
+                    puntos.add(new Punto(puntos.get(i).X()+cambioV,puntos.get(i).Y()));v--;}
+                else if((v>h && v-dt/2==0) ){
+                    puntos.add(new Punto(puntos.get(i).X()+cambioV,puntos.get(i).Y()));v--;}
+                else{
+                    puntos.add(new Punto(puntos.get(i).X(),puntos.get(i).Y()+cambioH));h--;}
+
             }
         }
-        puntos.add(puntoFinal);
-        return puntos;
+        else{
+            dt=h-v;
+                d=dt;
+            for(int i=0;i<a;i++){
+                
+                if(h-dt/2>v){
+                    puntos.add(new Punto(puntos.get(i).X(),puntos.get(i).Y()+cambioH));h--;}
+                else if((h>v && h-dt/2==0) ){
+                    puntos.add(new Punto(puntos.get(i).X(),puntos.get(i).Y()+cambioH));h--;}
+                else{
+                    puntos.add(new Punto(puntos.get(i).X()+cambioV,puntos.get(i).Y()));h--;}
+            }
+        }
 
+        puntos.add(PF);
+        return puntos ;
     }
 }
+
