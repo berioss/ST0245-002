a84c497724f50c7c8a96f0c5f8543edcca47aac1
berioss <68913258+berioss@users.noreply.github.com>
2020-08-20 19:39:37 -0500

    Update Punto3.java

--- talleres/taller04/Punto3.java
+++ talleres/taller04/Punto3.java
@@ -2,7 +2,8 @@
 public class Punto3 {
 
 	public static int fibonacci(int n) {
-		if(n==1||n==0)return 1;
+		if(n==1)return 1;
+		   if(n<=0)return 0;
 		else return fibonacci(n-1)+ fibonacci(n-2);
 		
 	}
