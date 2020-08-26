cedd3f76e3714f34b7b465b05ce3d14253004887
jsjuliod <68928790+jsjuliod@users.noreply.github.com>
2020-08-25 21:06:59 -0500

    Add files via upload

--- laboratorios/lab01/codigo/P1s1.java
+++ laboratorios/lab01/codigo/P1s1.java
@@ -1,32 +1,30 @@
 public class P1s1
 {
     public static void main(String[] args){
-        String adn1 = "abcdefg";
-        String adn2 = "acedfag";
+        String adn1 = "acb";
+        String adn2 = "abc";
         for(int i = 0; i < 20; i++){
-            adn1 += "g";
+            adn1 += "ac";
+            adn2 += "ab";
             long ti = System.currentTimeMillis();
-            invertir(subCadena2(adn1, adn2, adn1.length(), adn2.length()));
+            subCadena2(adn1, adn2, adn1.length(), adn2.length());
             long tf = System.currentTimeMillis();
             System.out.println(tf - ti);
         }
     }
 
-    public static String subCadena2(String str1, String str2, long len1, long len2){
+    public static int subCadena2(String str1, String str2, long len1, long len2){
         if(len1 == 0 || len2 == 0)
-            return "";
+            return 0;
         if(str1.charAt((int)len1-1) == str2.charAt((int)len2-1))
-            return str1.charAt((int)len1-1) + subCadena2(str1, str2, len1-1, len2-1);
+            return 1 + subCadena2(str1, str2, len1-1, len2-1);
         else{
-            String a = subCadena2(str1, str2, len1-1, len2);
-            String b = subCadena2(str1, str2, len1, len2-1);
-            return Math.max(a.length(), b.length()) == a.length() ? a : b;
+            int a = subCadena2(str1, str2, len1-1, len2);
+            int b = subCadena2(str1, str2, len1, len2-1);
+            return Math.max(a,b);
         }
     }
-
-    private static String invertir(String a){
-        if(a.length() <= 1)
-            return a;
-        return invertir(a.substring(1)) + a.charAt(0);
-    }
+    //Este codigo fue basado en el codigo presentado en la clase del Martes
+    //En verdad ya tenia esta misma estructura de codigo, solo que me confundi
+    //y pense que el metodo debia retornar la subcadena, y no solo su longitud
 }
