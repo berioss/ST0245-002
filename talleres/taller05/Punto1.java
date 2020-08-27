71aef67054b72ebf30429894dd0747aab1dc9f7a
berioss <68913258+berioss@users.noreply.github.com>
2020-08-27 13:12:17 -0500

    Update Punto1.java

--- talleres/taller05/Punto1.java
+++ talleres/taller05/Punto1.java
@@ -2,15 +2,27 @@ public class Punto1
 {
     public static int[] insertionSort(int[] a, int start){
         if(start < a.length-1){// c_1
-            if(start >= 0 && a[start] > a[start+1]){//c_2
+            for(int i=0;i>0;i++){
+            if(a[start] > a[start+1]){//c_2
+            }
                 int b = a[start];//c_3
                 a[start] = a[start+1];//c_4
                 a[start+1] = b;//c_5
             }
-            return insertionSort(insertionSort(a, start+1), start+1);//c_6+T(n-1)
+            }
+            return insertionSort(insertionSort(a, start+1), start+1);//c_6+T(n-1)*2
         }
         return a;
     }
+//Complejidad del algoritmo 
+//Ignoramos las constantes.
+//O( c_6 (2^n - 1) + c_1 2^(n - 1) ) Regla de la suma 
+//O(c_6 (2^n - 1)) Regla de la multiplicacion 
+//O(2^n - 1) Regla de la suma 
+//O(2^n)
+
+//Este algorimo no es practico para una alta cantidad de datos, ya que lo que se busca
+//con catidades enormes seria un crecimiento logaritmico, el exponencial no es adecuado 
 
     public static void main(String[] args){
         for(int n = 20; n < 30; n++){
