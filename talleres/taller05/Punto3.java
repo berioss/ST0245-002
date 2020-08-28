d92b6696790084315869ff399fcfdc5199519f18
jsjuliod <68928790+jsjuliod@users.noreply.github.com>
2020-08-27 22:42:18 -0500

    Add files via upload

--- talleres/taller05/Punto3.java
+++ talleres/taller05/Punto3.java
@@ -1,22 +1,28 @@
 public class Punto3
 {
     public static void mul(int n, int start){
-        int[] nums = {1,2,3,4,5,6,7,8,9,10};
-        if(start < nums.length && n != 0){
-            System.out.println(n+"x"+nums[start]+" = "+n*nums[start]);
-            mul(n, start+1);
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};//c_1
+        if(start < nums.length && n != 0){//c_2
+            System.out.println(n+"x"+nums[start]+" = "+n*nums[start]);//c_3
+            mul(n, start+1);//T(n-1)
         }
         else if(n > 0){
-            mul(n-1, 0);
-        }
-    }
+            mul(n-1, 0);//T(n-1)
+        }//T(n) = (c_1 + c_2 + c_3)n + c_1
+    }//T(n) es O(n)
 
     public static void main(String[] args){
-        for(int n = 1; n < 20; n++){
+        long[] a = new long[20];
+        int i = 0;
+        for(int n = 1; n < 200; n += 10){
             long ti= System.currentTimeMillis();
             mul(n, 0);
             long tf = System.currentTimeMillis();
-            System.out.println(tf-ti);
+            a[i] = tf-ti;
+            i++;
         }
+        for(int j = 0; j < a.length; j++){
+            System.out.println(a[j]);
+        }    
     }
 }
